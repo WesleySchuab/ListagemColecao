04eadae12e16e21b14794d6f04f3e3697e054728
Wesley Schuab <yinnihisoka@gmail.com>
2025-05-11 16:54:06 -0300

    Funcao Lambda com parametro

--- funcaocomoparametro/src/main/java/com/example/funcaocomoparametro/funcaoLambda.kt
+++ funcaocomoparametro/src/main/java/com/example/funcaocomoparametro/funcaoLambda.kt
@@ -3,8 +3,8 @@ package com.example.funcaocomoparametro
 
 fun main() {
 // Funcao Lambda
-    val funcaoLambda = {
-        println("Executar")
+    val funcaoLambda = { nome : String ->
+        println("Executar ${nome}")
     }
-    funcaoLambda()
+    funcaoLambda("Wesley")
 }
