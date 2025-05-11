091039af888caca79bb3e33e0c45daa419599f1d
Wesley Schuab <yinnihisoka@gmail.com>
2025-05-11 17:08:25 -0300

    Passando uma função comum para uma classe

--- funcaocomoparametro/src/main/java/com/example/funcaocomoparametro/funcaoLambda.kt
+++ funcaocomoparametro/src/main/java/com/example/funcaocomoparametro/funcaoLambda.kt
@@ -1,7 +1,15 @@
 package com.example.funcaocomoparametro
 
-
+class Botao {
+    fun configuirarCliquebotao(funcao: () -> Unit){
+    }
+}
+fun executarClique(){
+    println("executar clique")
+}
 fun main() {
+    val botao = Botao()
+    botao.configuirarCliquebotao (::executarClique)
 // Funcao Lambda
     val funcaoLambda = { nome : String ->
         println("Executar ${nome}")
